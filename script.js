ef42419d636cc44c3f6c280243cd5a3966da985f
hentschellj <hentschellj@gmail.com>
2020-02-06 15:22:58 -0600

    Quick error fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('.tabs-body .tab1').show().siblings().hide();
   $('.tabs-list a').on('click', function() {
-    let tabVal = '.tabs .' + $(this).data('tabs');
+    let tabVal = '.tabs-body .' + $(this).data('tabs');
     $(tabVal).slideDown().siblings().slideUp();
   });
 });
