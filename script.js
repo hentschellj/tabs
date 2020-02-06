47108bb60ac71dc972d41eadc07a3a3ae8d938c5
hentschellj <hentschellj@gmail.com>
2020-02-06 15:20:31 -0600

    Altered show() and hide() to slide methods on click

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ $(document).ready(function() {
   $('.tabs-body .tab1').show().siblings().hide();
   $('.tabs-list a').on('click', function() {
     let tabVal = '.' + $(this).data('tabs');
-    $(tabVal).show().siblings().hide();
+    $(tabVal).slideDown().siblings().slideUp();
   });
 });
