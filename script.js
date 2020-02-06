25a2677d543b001420ee80a08ddb79c30fa14ff1
hentschellj <hentschellj@gmail.com>
2020-02-06 15:18:36 -0600

    Added tab switching functionality

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
   $('.tabs-body .tab1').show().siblings().hide();
+  $('.tabs-list a').on('click', function() {
+    let tabVal = '.' + $(this).data('tabs');
+    $(tabVal).show().siblings().hide();
+  });
 });
